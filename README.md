5b2db8c94e2301539717d83bb2804971bddf70db
XabierGallardo <javiergallard91@gmail.com>
2026-02-05 17:16:00 -0300

    Added rotating 3d cube

--- README.md
+++ README.md
@@ -1,14 +1,49 @@
 # JS-3D-Graphics
 
+### Qué hace el código?
+
+- Dibujamos un **cubo 3D**
+- Lo **rotamos**
+- Lo **proyectamos en 2D**
+- Lo dibujamos en un `<canvas>` usando puntos y líneas
+
+No es 3D real con GPU, sino **3D simulado con matemáticas** (como los primeros videojuegos)
+
+
+![sample](sample.png)
+
+### Pipeline
+Para cada vértice:
+
+```
+3D original
+ ↓
+rotación
+ ↓
+traslación (z)
+ ↓
+proyección (3D → 2D)
+ ↓
+adaptación a pantalla
+ ↓
+dibujar
+```
+
+Este flujo es el mismo que usan con la GPU OpenGL, WebGL y los motores 3D
+
+---
+
+### Idea inicial
+
 Partiremos de una fórmula matemática que nos permitirá renderizar puntos 3D en nuestra pantalla
 
 Imaginemos un punto 3d en un espacio detrás de nuestra pantalla, para proyectar ese punto 3d en nuestra pantalla tendremos:
 
 ![formula](formula.png)
 
-
 Lo que nos devolverá esto será el punto proyectado en la pantalla.
 Si además tenemos un conjunto de puntos detrás de la pantalla en el espaico 3d y empezamos a animarlos y a rotarlos. Y usamos esta fórmula para renderizar todos estos puntos en nuestra pantalla, todo se sentirá como una escena 3d, como un objeto 3d
+
 
 ---
 
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const ctx = game.getContext("2d");
 console.log(ctx);
 
 // Para limpiar la pantalla la pintaremos de negro
+// Borramos el frame anterior, evitamos que queden rastros y lo ejecutamos en cada frame
 function limpiar() {
     ctx.fillStyle = fondoNegro;
     ctx.fillRect(0, 0, game.width, game.height);
@@ -22,6 +23,7 @@ function cubo({ x, y }) {
     ctx.fillStyle = verdeMatrix;
 
     // Consideramos las coordenadas en base a las dimensiones del cubo (s/2)
+    // Dibujamos cuadrados para representar vertices, objeto hecho de vertices
     ctx.fillRect(
         x - volumen/2, 
         y - volumen/2, 
@@ -29,6 +31,27 @@ function cubo({ x, y }) {
         volumen);
 }
 
+
+// Dibujamos lineas que unen los cubos (vertices), crean la forma del cubo y simulan las aristas
+function linea(p1, p2) {
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = verdeMatrix;
+    ctx.beginPath();
+    ctx.moveTo(p1.x, p1.y);
+    ctx.lineTo(p2.x, p2.y);
+    ctx.stroke();
+}
+
+
+/* Ajustamos al tamaño de la pantalla
+
+- En matemáticas tenemos valores pequeños -1 a 1
+- La pantalla trabaja en píxeles 0 a 800
+
+Con la funcion pantalla convertimos coordenadas matemáticas en coodenadas reales del canvas
+
+También invertimos el eje "y" porque en matemáticas "y" crece hacia arriba y en canvas hacia abajo
+*/
 
 // Adaptamos la formula al ancho game.width y al alto game.height
 function pantalla(p) {
@@ -41,6 +64,18 @@ function pantalla(p) {
 }
 
 
+/* De 3D a 2D
+
+- Canvas solo entiende x, y
+- Un punto 3D es x, y, z
+- En la funcion proyectar fingimos profundidad
+
+    Perspectiva:
+    - Si un objeto esta lejos (z grande) se ve pequeño
+    - Si está cerca (z pequeño) se ve grande
+
+*/
+
 // Implementamos la formula que toma un punto tridimensional
 function proyectar ({x, y, z}) {
     return { // Retorna un punto bidimensional
@@ -50,19 +85,89 @@ function proyectar ({x, y, z}) {
 }
 
 const FPS = 60;
-let dz = 0; // Toma el offset del punto z
+let dz = 1; // Toma el offset del punto z
+let angulo = 0;
 
+// Actualizamos el eje z con el dz
+// Movemos el cubo hacia adelante y o atras, simulamos profundidad
+function translate_z({x, y, z}) {
+    return {
+        x, y, z: z + dz
+    };
+}
+
+
+// Rotamos sobre el eje x
+// Giramos el cubo, alrededor de un eje usando seno y coseno
+function rotate_xz({x, y, z}, angulo) {
+    const c = Math.cos(angulo);
+    const s = Math.sin(angulo);
+
+    return {
+        x: x * c - z * s,
+        y,
+        z: x * s + z * c
+    }
+}
+
+// Vertices "vs", definimos los 8 puntos del cubo
+// Un cubo siempre tiene 8 vertices, 12 aristas y 6 caras
+const vs = [
+    // "Vanishing point"
+    // Estos cuatro puntos acercándose hacia el centro simulan un alejamiento
+    { x: 0.25, y: 0.25, z: 0.25 },
+    { x: -0.25, y: 0.25, z: 0.25 },
+    { x: -0.25, y: -0.25, z: 0.25 },
+    { x: 0.25, y: -0.25, z: 0.25 },
+
+    { x: 0.25, y: 0.25, z: -0.25 },
+    { x: -0.25, y: 0.25, z: -0.25 },
+    { x: -0.25, y: -0.25, z: -0.25 },
+    { x: 0.25, y: -0.25, z: -0.25 },
+];
+
+
+// Faces "fs"
+// Son indices, referencias a los vertices, un punto se conecta con otro formando las aristas del cubo
+const fs = [
+    // Array de indices para conectar como un poligono
+    [0, 1, 2, 3],
+    [4, 5, 6, 7],
+    [0, 4],
+    [1, 5],
+    [2, 6],
+    [3, 7]
+]
 
 function frame() {
     const dt = 1/FPS; // Delta time
-    dz += 1 * dt; // Incrementamos el offset
+    // dz += 1 * dt; // Incrementamos el offset y alejamos el elemento
+
+    angulo += Math.PI*dt;
     limpiar();
     
-    // Estos cuatro puntos acercándose hacia el centro simulan un alejamiento
-    cubo(pantalla(proyectar({ x: 0.5, y: 0.5, z: 1 + dz })));
-    cubo(pantalla(proyectar({ x: -0.5, y: 0.5, z: 1 + dz })));
-    cubo(pantalla(proyectar({ x: 0.5, y: -0.5, z: 1 + dz })));
-    cubo(pantalla(proyectar({ x: -0.5, y: -0.5, z: 1 + dz })));
+    // Proyectamos el punto 3d en una vista 2d y luego tomamos la vista 2d y la proyectamos en la plantalla
+
+    for (const v of vs) {
+        // Eliminamos las aristas
+        //cubo(pantalla(proyectar(translate_z(rotate_xz(v, angulo), dz))));
+    }
+
+    for (const f of fs) {
+        for(let i = 0; i < f.length; ++i) {
+            const a = vs[f[i]];
+            const b = vs[f[(i + 1) % f.length]];
+
+            const pa = pantalla(proyectar(translate_z(rotate_xz(a, angulo), dz)));
+
+            const pb = pantalla(proyectar(translate_z(rotate_xz(b, angulo), dz)));
+
+            // cubo(pa);
+            // cubo(pb);
+            linea(pa, pb) 
+
+        }
+    }
 
     setTimeout(frame, 1000/FPS);
 }
